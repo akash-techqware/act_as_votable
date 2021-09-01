7c1a14955bac92b0ba4267cff5f2cd8e751b8ea8
Akash KUmar <kumar.ryan94@gmail.com>
2021-09-01 20:07:57 +0530

    use of acts_as_votable gem

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'home#index'
+  resources :movies do
+    member do
+      put "like" => "movies#upvote"
+      put "dislike" => "movies#downvote"
+    end
+  end
+end
